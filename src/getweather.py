2a38a4952410d5b683b5f5cded18006a2987ac3b
Tandra Scourge <tandrasc@se-c12.nsk.21-school.ru>
2023-04-28 19:53:30 +0700

    file rnmd

---
+++ src/getweather.py
@@ -0,0 +1,27 @@
+import eel
+import pyowm
+from tabulate import tabulate
+owm = pyowm.OWM("178b3f1af37efa266b1b8ff3db366faa")
+
+@eel.expose
+def get_weather(city):
+	mrg = owm.weather_manager()
+
+	observation = mrg.weather_at_place(city)
+	w = observation.weather
+
+	temp =  w.temperature('celsius')['temp']
+	res = "В городе " + city + " сейчас " + str(temp) + " градусов"
+	data = [[city, str(temp)]]
+	col_name = ["Город", "Температура"]
+	print(tabulate(data, headers=col_name, tablefmt="fancy_grid"))
+
+	return res
+
+
+
+
+eel.init("browse")
+
+eel.start('code.html', size=(1000, 1000))
+
